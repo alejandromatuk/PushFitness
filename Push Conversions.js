4c7071a3a1077dc468d42b10974a2e2e4d60d7a9
alejandromatuk <111020525+alejandromatuk@users.noreply.github.com>
2023-10-10 16:22:09 -0400

    Add files via upload

---
+++ Push Conversions.js
@@ -0,0 +1,20 @@
+<!-- Event snippet for Lead Submission conversion page -->
+<script>
+  gtag('event', 'conversion', {'send_to': 'AW-315103963/23nUCO6S2uoYENu1oJYB'});
+</script>
+<!-- Event snippet for Lead Submission conversion page -->
+<script>
+  gtag('event', 'conversion', {'send_to': 'AW-10801633728/JietCPa34-oYEMCzz54o'});
+</script>
+<!-- Event snippet for Lead Submission conversion page -->
+<script>
+  gtag('event', 'conversion', {'send_to': 'AW-315144632/uFDuCN2P4-oYELjzopYB'});
+</script>
+<!-- Event snippet for Lead Form Submission conversion page -->
+<script>
+  gtag('event', 'conversion', {'send_to': 'AW-731481031/4-2xCNyu3-oYEMeH5twC'});
+</script>
+<!-- Event snippet for Sign-up conversion page -->
+<script>
+  gtag('event', 'conversion', {'send_to': 'AW-315677605/qHeeCKyshuQDEKW3w5YB'});
+</script>
